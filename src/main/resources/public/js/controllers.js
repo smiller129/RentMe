ba149fd5d6c560c8b674854a14c27ebc6c446e36
jwentz02 <jtwentz@hotmail.com>
2016-04-20 20:35:40 -0400

    Added itemController

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -1,5 +1,10 @@
 /**
  * Angular Controllers.  Used to communicate with the server.
+ * 
+ * Revision history:
+ * 
+ * 4/5/16 - Dan Rogers - Initial Version
+ * 4/20/16 - Jeff Wentz - Added itemController
  */
 rentmeApp.controller('greetingController', ['$scope', '$http',
   function ($scope, $http) {
@@ -15,4 +20,18 @@ rentmeApp.controller('aboutController', ['$scope', '$http',
 			.success(function(data) {
 				$scope.members = data;
 			});
+}]);
+
+rentmeApp.controller('itemController', ['$scope', '$http',
+    function ($scope, $http) {
+		$http.get('/items')
+			.success(function(data) {
+				$scope.items = data;
+			});
+		/* TODO: Add
+		/item
+		/reserveItem
+		/updateItem
+		/admin-update-item
+		*/
 }]);
